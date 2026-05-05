5a4cac77dcb34606a1abc9af5c6b3579435d8eff
saumyabhartikarn <saumyabhartikarn@gmail.com>
2026-05-05 15:31:00 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,5 @@
-const CACHE_NAME = "kavita-app-v3";
-
-// INSTALL
-self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon.png"
-      ]);
-    })
-  );
+self.addEventListener("install", e => {
+  console.log("Service Worker Installed");
 });
 
-// ACTIVATE
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// FETCH
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("fetch", e => {});
